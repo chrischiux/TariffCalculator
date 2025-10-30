f162f31f24785e4047bc0634b50f1cd992a8c8c0
Chris Chiu <sc22phc@leeds.ac.uk>
2025-10-30 18:59:39 +0000

    added base url build action variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: process.env.VITE_BASE_URL || '/', // use env var or fallback
+    plugins: [react()],
     server: {
-      fs: {
+        fs: {
 
-          allow: ['.','C:\\Users\\propo\\node_modules']
-      }
+            allow: ['.', 'C:\\Users\\propo\\node_modules']
+        }
     }
 })
